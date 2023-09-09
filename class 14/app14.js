4d859e1f0b4603980db5cdb2700395ea221c3fda
Haseebismail703 <haseebismail703@gmail.com>
2023-09-09 20:09:16 +0500

    hi

--- class 14/app14.js
+++ class 14/app14.js
@@ -48,21 +48,21 @@
 
 
 
-let button = document.getElementById("button")
-let img = document.getElementById("bulb")
-let abc = "on"
+// let button = document.getElementById("button")
+// let img = document.getElementById("bulb")
+// let abc = "on"
 
-button.addEventListener("click", () => {
-  if (abc === "on") {
+// button.addEventListener("click", () => {
+  // if (abc === "on") {
     // button.innerText = "OFF";
-    img.src = "https://www.iconarchive.com/download/i136733/microsoft/fluentui-emoji-3d/Light-Bulb-3d.1024.png"
-   abc = "off"
-  } else {
+    // img.src = "https://www.iconarchive.com/download/i136733/microsoft/fluentui-emoji-3d/Light-Bulb-3d.1024.png"
+  //  abc = "off"
+  // } else {
     // button.innerText = "ON";
-    img.src = "https://previews.123rf.com/images/www3d/www3d1711/www3d171100001/89319097-3d-illustration-of-light-bulb-isolated-on-white-background-realistic-3d-rendering-of-incandescent.jpg"
-    abc = "on"
-  }
-}) 
+//     img.src = "https://previews.123rf.com/images/www3d/www3d1711/www3d171100001/89319097-3d-illustration-of-light-bulb-isolated-on-white-background-realistic-3d-rendering-of-incandescent.jpg"
+//     abc = "on"
+//   }
+// }) 
 
 //  ternanry operator 
 
@@ -70,6 +70,14 @@ button.addEventListener("click", () => {
 
 // var b = (a==5? 'sahi hai' : 'galat hai' )
 // document.write(b)
+
+
+
+
+
+
+
+
 
 
 
