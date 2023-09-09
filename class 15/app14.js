f403a1f5dcc9a545a79b0421f8fc58c035601c66
Haseebismail703 <haseebismail703@gmail.com>
2023-09-09 22:32:07 +0500

    halfclass done

--- class 15/app14.js
+++ class 15/app14.js
@@ -1,28 +1,95 @@
-var arrofobj = [
+// var arrofobj = [
 
-    {
-        p_Name : 'Sumsung',
-        p_id :  '01',
-        p_price :  '50000',
-        p_image :'https://images.priceoye.pk/samsung-galaxy-z-fold4-pakistan-priceoye-lsd5k-270x270.webp'
-    },
-    {
-        p_Name : 'Sumsung',
-        p_id :  '02',
-        p_price :  '60000',
-        p_image :'https://images.priceoye.pk/samsung-galaxy-z-fold4-pakistan-priceoye-lsd5k-270x270.webp'
-    },
+//     {
+//         p_Name : 'Sumsung',
+//         p_id :  '01',
+//         p_price :  '50000',
+//         p_image :'https://images.priceoye.pk/samsung-galaxy-z-fold4-pakistan-priceoye-lsd5k-270x270.webp'
+//     },
+//     {
+//         p_Name : 'Sumsung',
+//         p_id :  '02',
+//         p_price :  '60000',
+//         p_image :'https://images.priceoye.pk/samsung-galaxy-z-fold4-pakistan-priceoye-lsd5k-270x270.webp'
+//     },
     
 
 
-]
+// ]
+
+
+
+// for (var arrofobj=0; arrofobj.length;  )
+// document.write(arrofobj[0])
+// var getimg = document.getElementById('img')
+// getimg.setAttribute('class','img')
+// getimg.src = arrofobj[0].p_image
+
+
+
 
 
 // document.write(arrofobj[0].p_image)
 
-var getimg = document.getElementById('img')
-getimg.setAttribute('class','img')
-getimg.src = arrofobj[0].p_image
 
 
 
+
+
+var question = [
+
+    {
+        quetion1 : 'What is the sum of 2+2',
+        ans1 : '2',
+        ans2: '4',
+        ans3: '6',
+        ans4 : '5'
+
+    },
+
+    {
+        quetion1 : 'What is the - of 2-2',
+        ans1 : '1',
+        ans2: '2',
+        ans3: '5',
+        ans4 : '0'
+    }
+]
+
+var getque = document.getElementById('que')
+var getans1 = document.getElementById('ans1')
+var getans2 = document.getElementById('ans2')
+var getans3 = document.getElementById('ans3')
+var getans4 = document.getElementById('ans4')
+
+//  getque.innerHTML =   question[0].quetion1
+//  getans1.innerHTML =  question[0].ans1
+//  getans2.innerHTML =  question[0].ans2
+//  getans3.innerHTML =  question[0].ans3
+//  getans4.innerHTML =  question[0].ans4
+ 
+ var index = 0 
+
+
+
+
+ function nextQuestion(){
+
+     if(index > question.length -1 ){
+        console.log('khatam')
+        document.write('youre quize score')
+     }
+
+   else{
+    getque.innerHTML =   question[index].quetion1
+    getans1.innerHTML =  question[index].ans1
+    getans2.innerHTML =  question[index].ans2
+    getans3.innerHTML =  question[index].ans3
+    getans4.innerHTML =  question[index].ans4
+    index++
+   }
+   
+ }
+
+
+ nextQuestion()
