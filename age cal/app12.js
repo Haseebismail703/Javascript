52014380d15412ec2bec3dbac0ab81c50ed9719f
Haseeb <139354115+Haseebismail703@users.noreply.github.com>
2023-09-11 18:01:11 +0500

    Update

--- age cal/app12.js
+++ age cal/app12.js
@@ -54,3 +54,140 @@ document.write(Math.floor(formula))
 
 
 
+                                      //  question no 1 
+
+// var a = prompt('Enter youre name')
+// alert(a)
+
+
+
+                                       // question no 2
+
+
+
+// var a = +prompt('enter table no')
+
+
+// for(i=1; i<=10;i++){
+//     document.write(a+"x"+i+"="+a*i+ "<br>")
+//     if(a==''){
+//         for(i=1; i<=10;i++){
+//             document.write(5+"x"+i+"="+5*i+ "<br>")
+//         }
+
+       
+//     }
+
+// }
+
+
+
+                //  question no 3
+
+
+
+//   var a = prompt('enter a city name')     
+  
+  
+//   if (a == 'Karachi'){
+//     alert("Welcome to the city of light")
+//   }
+ 
+
+
+
+    //  question no 4
+
+
+    // var a = prompt('enter youre gender')
+
+    // if(a=="male" ){
+    //     alert('Good morning sir')
+    // }
+    // else if (a=='female'){
+    //     alert('Good morning Ma,am')
+    // }
+
+    // else(
+    //     alert('enter youre gender')
+    // )
+  
+
+
+    // question no 5
+
+
+
+
+    
+var a = prompt('enter value')
+if(a =='red')
+{
+    document.write(
+    "<table border='3px'>"+
+    " <tr>"+
+        "<th>"+"signal coloure"+"</th>"+
+    "<th>"+"Massage"+"</th>"+
+ "</tr>"+
+    " <td>"+
+         'red'+
+     "</td>"+
+     " <td>"+
+         'vehicle should stop'+
+     "</td>"+
+ 
+ 
+                        "</table>"
+    )
+}
+else if (a=='yellow'){
+    document.write(
+
+
+
+
+    
+    "<table border='3px'>"+
+    " <tr>"+
+        "<th>"+"signal coloure"+"</th>"+
+    "<th>"+"Massage"+"</th>"+
+ "</tr>"+
+    " <td>"+
+         'Yellow'+
+     "</td>"+
+     " <td>"+
+         'redy to move'+
+     "</td>"+
+ 
+ 
+                        "</table>"
+
+    )
+}
+else if (a=='green') {
+
+
+document.write(
+
+             "<table border='3px'>"+
+   " <tr>"+
+       "<th>"+"signal coloure"+"</th>"+
+   "<th>"+"Massage"+"</th>"+
+"</tr>"+
+   " <td>"+
+        'green'+
+    "</td>"+
+    " <td>"+
+        'Vehicle can move now'+
+    "</td>"+
+
+
+                       "</table>"
+
+)
+
+}
+else(
+    alert('no')
+)
+
