7ba5bf96eafaac8e58701c86d3561c5c056371dc
Haseebismail703 <haseebismail703@gmail.com>
2023-09-12 12:42:05 +0500

    hi

---
+++ class 2/app.js
@@ -0,0 +1,180 @@
+
+                                      //  question no 1 
+
+// var a = prompt('Enter youre name')
+// alert(a)
+
+
+
+                                       // question no 2
+
+
+
+// var a = +prompt('enter table no')
+
+
+// for(i=1; i<=10;i++){
+//     document.write(a+"x"+i+"="+a*i+ "<br>")
+//     if(a==''){
+//         for(i=1; i<=10;i++){
+//             document.write(5+"x"+i+"="+5*i+ "<br>")
+//         }
+
+       
+//     }
+
+// }
+
+
+
+                //  question no 3
+
+
+
+//   var a = prompt('enter a city name')     
+  
+  
+//   if (a == 'Karachi'){
+//     alert("Welcome to the city of light")
+//   }
+ 
+
+
+
+    //  question no 4
+
+
+    // var a = prompt('enter youre gender')
+
+    // if(a=="male" ){
+    //     alert('Good morning sir')
+    // }
+    // else if (a=='female'){
+    //     alert('Good morning Ma,am')
+    // }
+
+    // else(
+    //     alert('enter youre gender')
+    // )
+  
+
+
+    // question no 5
+
+
+
+
+    
+    // var a = prompt('enter value')
+    // if(a =='red')
+    // {
+    //     document.write(
+    //     "<table border='3px'>"+
+    //     " <tr>"+
+    //         "<th>"+"signal coloure"+"</th>"+
+    //     "<th>"+"Massage"+"</th>"+
+    //  "</tr>"+
+    //     " <td>"+
+    //          'red'+
+    //      "</td>"+
+    //      " <td>"+
+    //          'vehicle should stop'+
+    //      "</td>"+
+     
+     
+    //                         "</table>"
+    //     )
+    // }
+    // else if (a=='yellow'){
+    //     document.write(
+    
+    
+    
+    
+        
+    //     "<table border='3px'>"+
+    //     " <tr>"+
+    //         "<th>"+"signal coloure"+"</th>"+
+    //     "<th>"+"Massage"+"</th>"+
+    //  "</tr>"+
+    //     " <td>"+
+    //          'Yellow'+
+    //      "</td>"+
+    //      " <td>"+
+    //          'redy to move'+
+    //      "</td>"+
+     
+     
+    //                         "</table>"
+    
+    //     )
+    // }
+    // else if (a=='green') {
+    
+    
+    // document.write(
+    
+    //              "<table border='3px'>"+
+    //    " <tr>"+
+    //        "<th>"+"signal coloure"+"</th>"+
+    //    "<th>"+"Massage"+"</th>"+
+    // "</tr>"+
+    //    " <td>"+
+    //         'green'+
+    //     "</td>"+
+    //     " <td>"+
+    //         'Vehicle can move now'+
+    //     "</td>"+
+    
+    
+    //                        "</table>"
+    
+    // )
+    
+    // }
+    // else(
+    //     alert('no')
+    // )
+
+
+
+                        // question no 6 
+
+
+
+
+    //    var a = prompt('enter youre age')
+       
+    //    if(a>17){
+    //     alert(' you are Welcome')
+    //    }
+    //    else(
+    //     alert('you are not 18 year')
+    //    )
+
+
+
+
+                //   question no 7 
+
+
+    //    var a = prompt('enter a no ')
+       
+    //    if(a<0.24){
+    //     alert('plese refill the feul in the car')
+    //    }
+    //    else(
+    //     alert('Tanki full')
+    //    )
+        
+                   // question no 8
+
+
+// var a = 4
+
+// if(++a === 5){
+//     alert('yes')
+// }
+// else(
+//     alert('no')
+// )
