df48ee69e36c87f3e097e24e4800ad8121953b17
Haseebismail703 <haseebismail703@gmail.com>
2023-09-14 22:38:37 +0500

    hi

---
+++ ass/app.js
@@ -0,0 +1,110 @@
+                //   Q no 1
+// var i ;
+
+// for(i=1;  i<=5; i++ ){
+//      document.write('Heloo world',"<br>")
+// }
+
+
+
+        //    Q no 2 
+
+
+//         var i ;
+
+// for(i=1;  i<=10; i++ ){
+//      document.write(i,'<br>')
+// }
+
+
+//    Q no 3 
+
+
+
+// var a = +prompt('Enter Table')
+// var b = +prompt('Enter last table no')
+// for(i=1; i<=b; i++ ){
+//     document.write(a+"x"+i+"="+a*i+"<br>")
+// }
+
+
+
+
+//    Q no 4 
+
+
+//     var arr = ['Sumsung','Iphone','MOto']
+// for(var i=0; i<arr.length; i++ ){
+//     document.write(arr[i],"<br>")
+// }
+
+
+
+                    //    Q no 1
+                    
+
+                    
+// var a = 'Haseeb'
+// var b ='Ismail' 
+
+// var fullname = 'Haseeb ismail'
+
+// document.write(fullname)
+
+
+
+// Q no 2 
+
+// var a = 'My favouret phone is:'
+// document.write(a)
+// var b = prompt('Enteryoure fav mobile name')
+// document.write(b)
+
+// document.write('<br>')
+// var c = b.length
+// document.write('length of string',c)
+
+
+
+// Q no 3 
+
+// document.write('String:pakistan')
+// document.write(:'<br>')
+// document.write('Index of n')
+// var a = "string: pakistan"
+// var b = a.indexOf('n')
+// document.write(b)
+
+
+    //  Q no 4
+
+// document.write('String:pakistan')
+// document.write('<br>')
+// document.write('Index of n')
+// var a = "string: pakistan"
+// var b = a.lastIndexOf('n')
+// document.write(b)
+
+
+
+// Q no 8 
+
+
+// var a = 'Lorem ipsum dolor, sit amet and  consectetur adipisicing elit. Illo and perferendis iste fuga'
+
+
+// var b = a.replace('and','&')
+
+// document.write(b)
+
+
+// Q no 9
+
+// var a = 'Haseeb'
+// document.write(a,'<br>',typeof(a))
+
+// var b = 123
+// document.write('<br>')
+// document.write(b,'<br>',typeof(b))
+
+
