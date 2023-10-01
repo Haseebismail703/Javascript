6c85ecd5ada9c244122ca7c3bbe3bb63d4af4f30
Haseebismail703 <haseebismail703@gmail.com>
2023-10-01 19:58:15 +0500

    go

--- Todo app/app.js
+++ Todo app/app.js
@@ -21,27 +21,65 @@
 //     var b = a.setAttribute('class','aaa')
 //     console.log(b)
 // }
+// var getul = document.getElementById('ul')
+
+// function clicke(){
+  
+
+//     var a = document.getElementById('inp')
+//     var li = document.createElement("li")
+//     var textli = document.createTextNode(a.value)
+
+//     li.appendChild(textli)
+//     getul.appendChild(li)
+//     a.value = ''
+
+   
+// }
+
+// function Deletall() {
+//    getul.innerHTML=' '
+// }
+
 var getul = document.getElementById('ul')
 
 function clicke(){
-  
+
 
     var a = document.getElementById('inp')
-    var li = document.createElement("li")
+    var li = document.createElement('li')
     var textli = document.createTextNode(a.value)
-
     li.appendChild(textli)
     getul.appendChild(li)
     a.value = ''
 
-    // if (a.value ==''){
-    //     alert('Enter value')
-    // }
+    //  del btn 
+    
+    var Deletbtn = document.createElement('button')
+    var textlidel = document.createTextNode('Delete')
+    Deletbtn.appendChild(textlidel)
+    li.appendChild(Deletbtn)
+    Deletbtn.setAttribute('onclick','del(this)')
+
+    // edittext 
+
+    var edits = document.createElement('button')
+    var edittext = document.createTextNode('Edit')
+    edits.appendChild(edittext)
+    li.appendChild(edits)
+   edits.setAttribute('onclick','edit(this)')
+
 }
 
-function Deletall() {
-   getul.innerHTML=''
+function Deletall(){
+    getul.innerHTML= ''
 }
 
+function del(e){
+    e.parentNode.remove()
+}
 
-
+function edit(e){
+    var a = prompt('Enter value')
+    e.parentNode.firstChild.nodeValue = a
+}
