529f812a26fbe594d83b47fc0d6fab4320b49d27
Haseebismail703 <haseebismail703@gmail.com>
2023-10-07 23:11:20 +0500

    ass

---
+++ Assigment/app.js
@@ -0,0 +1,196 @@
+
+//  q no 1
+
+
+
+// let number1 = prompt('Enter a value')
+// document.write('number :',' ',number)
+
+ 
+
+// document.write( '<br>','round of value of the number :'," " ,Math.round(number1) )
+
+
+
+// document.write( '<br>','floor value of the number :'," " ,Math.floor(number1) )
+
+
+// document.write( '<br>','floor value of the number :'," " ,Math.ceil(number1) )
+
+
+//  Q no 2
+
+// let number2 = prompt('Enter a value')
+// document.write('number :',' ',number2)
+
+ 
+
+// document.write( '<br>','round of value of the number :'," " ,Math.round(number2) )
+
+
+
+// document.write( '<br>','floor value of the number :'," " ,Math.floor(number2) )
+
+
+// document.write( '<br>','floor value of the number :'," " ,Math.ceil(number2) )
+
+//   Q no 3
+
+// let number3 = prompt('Enter a value')
+// document.write('number :',' ',number3)
+
+ 
+
+// document.write( '<br>','round of value of the number :'," " ,Math.round(number3) )
+
+
+
+// document.write( '<br>','floor value of the number :'," " ,Math.floor(number3) )
+
+
+// document.write( '<br>','floor value of the number :'," " ,Math.ceil(number3) )
+
+
+
+//  Q no 4
+
+// let number4 = prompt('Enter a value')
+// document.write('number4 :',' ',number4)
+
+ 
+
+// document.write( '<br>','round of value of the number :'," " ,Math.round(number4) )
+
+
+
+// document.write( '<br>','floor value of the number :'," " ,Math.floor(number4) )
+
+
+// document.write( '<br>','floor value of the number :'," " ,Math.ceil(number4) )
+
+
+
+// Q no 8
+
+
+//  document.write( '<br>','random number 1 & 100 :'," " ,Math.random()*100 )
+
+// Q no 9
+
+
+// let number4 = prompt('Enter youre weight in kg')
+
+// document.write('The weight of user is :',' ',number4)
+
+//   Q no 10
+
+// let num = prompt('Enter a value')
+// let num2 = '7'
+
+// if (num == num2){
+//     alert('congratulation')
+// }
+// else(
+//     alert('Try again')
+// )
+
+
+
+// Q no 11
+
+// let date = Date()
+// document.write(date)
+
+// Q no 12
+// let date = Date().slice(4,7)
+// document.write("current month:",date)
+
+
+// Q no 13
+
+// let date = Date().slice(0,3)
+// // alert('Today is ',date)
+// document.write('Today is ',date)
+
+// Q no 14 
+
+// let date = Date().slice(0,3)
+// // alert('Today is ',date)
+// document.write('Its funday ',date)
+
+// Q no 15
+
+// let date = new Date()
+// let getdate = date.getDate()
+
+// if(date>=16){
+//     document.write('first fifteen day of month')
+// }
+// else(
+//     document.write('Last fifteen day of month')
+// )
+
+// Q no 16 
+
+// let date = new Date()
+// document.write(date)
+
+
+//  let getdate = date.getTime()
+//  document.write('<br>','Elapsedmilisec in january 1970:',getdate)
+
+//  let getdate2 = date.getUTCMinutes()
+//  document.write('<br>','Elapsedmilisec in january 1970:',getdate2)
+
+
+// Q no 17 
+
+// let date = new Date()
+// let getDate = date.getHours()
+
+// if(getDate>=12){
+//     alert('its PM')
+// }
+// else(
+//     alert(' its AM')
+// )
+
+
+
+// Q no 19
+
+// let laterDate = new Date('31 Dec,2020')
+// document.write(laterDate)
+
+// Q no 20
+
+
+
+
+// let now = new Date()
+
+
+// let ramadanStartDate = new Date(2015, 5, 18)
+
+
+// let daysPast = Math.floor((now - ramadanStartDate) / (1000 * 60 * 60 * 24))
+
+// alert(`Number of days past since 1st Ramadan: ${daysPast}`)
+
+// Q no 21 
+
+
+// let date = new Date()
+
+// let datemili = date.getSeconds()
+
+// let foo = date - datemili
+// document.write(foo)
+
+
+// Q no 21
+
+
+
+
+
--- calculator/app9.js
+++ calculator/app9.js
@@ -43,4 +43,18 @@ function cl(){
     a.value = a.value.slice (0,-1)
 }
 
+// function abc(e){
+//    document.getElementById('inp').value += e
+// }
+// function eq(){
+//     var a = document.getElementById('inp')
+//     a.value = eval (a.value)
+// }
 
+// function clr(){
+//    document.getElementById('inp').value=""
+// }
+// function cl(){
+//    var a = document.getElementById('inp')
+//     a.value = a.value.slice (0,-1)
+// }
