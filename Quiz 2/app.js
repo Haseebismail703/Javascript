a8b0dbc42e018e60abf545490e229b17abaa8a49
Haseebismail703 <haseebismail703@gmail.com>
2023-10-19 23:24:27 +0500

    quiz

---
+++ Quiz 2/app.js
@@ -0,0 +1,85 @@
+      
+    //   chap no 21
+
+
+    // let pro = prompt('Enter value 1')
+
+    // let pro1 = pro.slice(0,1).toUpperCase()
+    // let pro2 = pro.slice(1).toLowerCase()
+    // let res = pro1 + pro2
+    // document.write(res)
+
+    //   chp no 22
+
+    // index off
+
+    // let arr = ['saad','Haseeb','saad']
+    // document.write(arr.indexOf('saad',1))
+       
+    
+    // let arr = ['saad','Haseeb','saad']
+    // document.write(arr.lastIndexOf('saad'))
+
+
+    //  chartat
+
+    // let arr = 'REd'
+
+    // document.write(arr.charAt(3))
+
+
+
+    //  math.round .5 pa change kardaga
+    // let math = '72.5'
+
+    // document.write(Math.round(math))
+
+
+//    math .ceil aga lajaiga 72 ko 73 kardaga 
+
+    // let math = '72.6'
+
+    // document.write(Math.ceil(math))
+
+
+    //  math .floor change nhi kara ga 
+
+
+    // let math = '72.6'
+
+    // document.write(Math.floor(math))
+
+
+
+    
+//    math.random agar 1 likhoga to 0 sa uthaiga 2 likho ga to 0 sa 1 tak uthaiga
+    // document.write(Math.random()*2)
+
+
+    // task
+
+
+    // let player1 = 'Baber'
+    // let player2 = 'kohli'
+    
+    // let math = Math.random()*2
+    // let floor = Math.floor(math)
+
+    // console.log(floor)
+    // if(floor == 1){
+    //     document.write('kohli win')
+
+    // }
+    // else{
+    //     document.write('Baber wins')
+    // }
+
+
+
+
+  
+
+
+
+
+    
