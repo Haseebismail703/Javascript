c553efcb28b76e215ddd646ab9e9d6ee04722ebd
Haseebismail703 <haseebismail703@gmail.com>
2023-10-25 20:08:24 +0500

    ji

---
+++ Quiz 2/chap4.js
@@ -0,0 +1,33 @@
+// What is the string "Hello, world!" with the first character replaced with an "A"?
+// What is the string "This is a longer string." with all of the vowels replaced with asterisks?
+// What is the string "Hello, world!" with all of the consonants replaced with spaces?
+// What is the string "This is a longer string." with the last character removed?
+// What is the string "Hello, world!" with the first three characters removed?
+// What is the string "This is a longer string." with the substring "a longer" replaced with "a shorter"?
+// What is the string "Hello, world!" with all of the occurrences of the character "l" replaced with the character "r"?
+// What is the string "This is a longer string." with all of the occurrences of the character "s" replaced with the character "z"?
+// What is the string "Hello, world!" with all of the characters except for the vowels removed?
+// What is the string "This is a longer string." with all of the characters except for the consonants removed?
+// Answers:
+
+// "Aello, world!"
+// "Ths s * lngr str*ng."
+// " e l l o , o r l d ! "
+// "This is a longer strin"
+// "world!"
+// "This is a shorter string."
+// "Herro, wrrd!"
+// "Thiz iz a lonzer ztring."
+// "eao"
+// "Ths s lngr strng"
+
+
+
+// var a = 'Heloo world'
+
+// var b = a.replace('H','A')
+
+// document.write(b)
+
+
+
