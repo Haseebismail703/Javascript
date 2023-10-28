6d812851f63ed8ac87be757fcf37e98b2b3a10c6
Haseebismail703 <haseebismail703@gmail.com>
2023-10-28 21:50:29 +0500

    gh

---
+++ Quiz 2/chap19.js
@@ -0,0 +1,15 @@
+
+
+
+// var a = '4'
+// switch(a){
+//     case'5':
+//     document.write('han 5 hai')
+//   break
+//     case '4':
+//         document.write('4 hai ')
+
+// break
+//         default:
+//       document.write  ('invalid')
+// }
